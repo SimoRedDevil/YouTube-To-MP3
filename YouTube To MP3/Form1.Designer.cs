5137f02c03d04d52c6a091e8d3bc5eb69ab5419c
simoely <82889071+simoely@users.noreply.github.com>
2021-04-26 17:12:20 +0000

    Add files via upload

---
+++ YouTube To MP3/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace YouTube_To_MP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblDuration = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Link :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(366, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 27);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Format :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "MP3 64kbps",
+            "MP3 128kbps",
+            "MP3 192kbps",
+            "MP3 256kbps",
+            "MP3 320kbps"});
+            this.comboBox1.Location = new System.Drawing.Point(67, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 25);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "MP3 64kbps";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(335, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 27);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Download";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(6, 21);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(59, 17);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Status : -";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(8, 67);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(411, 14);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // lblDuration
+            // 
+            this.lblDuration.AutoSize = true;
+            this.lblDuration.Location = new System.Drawing.Point(6, 43);
+            this.lblDuration.Name = "lblDuration";
+            this.lblDuration.Size = new System.Drawing.Size(74, 17);
+            this.lblDuration.TabIndex = 8;
+            this.lblDuration.Text = "Duration : -";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.lblDuration);
+            this.groupBox1.Controls.Add(this.lblStatus);
+            this.groupBox1.Location = new System.Drawing.Point(8, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(425, 92);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Informations";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 169);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YouTube To MP3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblDuration;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ YouTube To MP3/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace YouTube_To_MP3
+{
+    public partial class Form1 : Form
+    {
+        public WebBrowser webRequest = new WebBrowser();
+        public string mainURL = "https://youtubetomp3music.com/en7/download?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D";
+        public int MP3 = 0;
+        public string per = "";
+        public string mp3Title = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webRequest.ScriptErrorsSuppressed = true;
+            webRequest.Navigate(mainURL);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblStatus.Text = "Status : -"; 
+            lblStatus.Text = "Duration : -";
+            progressBar1.Value = 0;
+
+            string[] urlSplit = textBox1.Text.Split(new char[] { '=' }, StringSplitOptions.None);
+            string fullURL = mainURL + urlSplit[1];
+            if(comboBox1.Text == "MP3 64kbps")
+            {
+                MP3 = 0;
+            }
+            else if(comboBox1.Text == "MP3 128kbps")
+            {
+                MP3 = 1;
+            }
+            else if (comboBox1.Text == "MP3 192kbps")
+            {
+                MP3 = 2;
+            }
+            else if (comboBox1.Text == "MP3 256kbps")
+            {
+                MP3 = 3;
+            }
+            else if(comboBox1.Text == "MP3 320kbps")
+            {
+                MP3 = 4;
+            }
+            
+            webRequest.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(DocumentCompleted);
+            webRequest.Navigate(fullURL);
+            lblStatus.Text = "Status : Starting...";
+        }
+
+        public void DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            webRequest.Document.GetElementById("formatselect").GetElementsByTagName("optgroup")[1].GetElementsByTagName("option")[MP3].SetAttribute("selected", "selected");
+            webRequest.Document.GetElementById("cvt-btn").InvokeMember("click");
+            string startConverting = webRequest.Document.GetElementById("cvt-btn").InnerHtml;
+            mp3Title = webRequest.Document.GetElementsByTagName("h3")[0].InnerHtml;
+            lblDuration.Text = webRequest.Document.GetElementsByTagName("p")[0].InnerHtml.Replace(": ", " : ");
+
+            if (startConverting == "Starting...")
+            {
+                lblStatus.Text = "Status : Converting...";
+                timer1.Start();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string downloadLink = webRequest.Document.GetElementById("mp3-dl-btn").GetAttribute("href");
+            WebClient WC = new WebClient();
+            WC.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DownloadProgressChanged);
+            WC.DownloadDataCompleted += new DownloadDataCompletedEventHandler(DownloadDataCompleted);
+            WC.DownloadDataAsync(new Uri(downloadLink));
+        }
+
+        public void DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        public void DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();       
+            saveFile.Filter = "MP3 File | *.mp3";
+            saveFile.FileName = mp3Title + ".mp3";
+            saveFile.ShowDialog();
+            byte[] downloadedBytes = e.Result;
+            System.IO.File.WriteAllBytes(saveFile.FileName, downloadedBytes);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            per =  webRequest.Document.GetElementById("cvt-process").InnerHtml;
+            string btnConvert = webRequest.Document.GetElementById("cvt-btn").InnerHtml;
+            if (btnConvert == "Convert")
+            {
+                timer1.Stop();
+                lblStatus.Text = "Status : Converted Successfully !";
+            }
+        }
+    }
+}
